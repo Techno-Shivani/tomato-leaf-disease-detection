e963b9944c044b308371ad1bf8b3654711154a6c
Shivani Chauhan <sc622732@gmail.com>
2026-05-12 09:47:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,30 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import os
-
-st.set_page_config(page_title="Tomato Leaf Disease Detection")
+import gdown
 
 MODEL_PATH = "best_model.keras"
+
+# Download model from Google Drive
+if not os.path.exists(MODEL_PATH):
+    url = "https://drive.google.com/uc?id=1pflxySlxBXUmLVOorwLU93MzJVHM16ub"
+    gdown.download(url, MODEL_PATH, quiet=False)
 
 # Load model
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model(MODEL_PATH)
-    return model
+    return tf.keras.models.load_model(MODEL_PATH)
 
 model = load_model()
 
-# Load class names
+# Class names
 with open("class_names.txt", "r") as f:
     class_names = [line.strip() for line in f.readlines()]
 
-# Title
 st.title("🍅 Tomato Leaf Disease Detection")
 
-st.write("Upload a tomato leaf image to detect disease.")
-
 uploaded_file = st.file_uploader(
-    "Choose an image",
+    "Upload Tomato Leaf Image",
     type=["jpg", "jpeg", "png"]
 )
 
@@ -36,18 +36,14 @@ if uploaded_file is not None:
 
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    # Preprocess image
     img = image.resize((224, 224))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
 
-    # Prediction
-    prediction = model.predict(img_array)
+    prediction = model.predict(img)
 
     predicted_class = class_names[np.argmax(prediction)]
-
     confidence = np.max(prediction) * 100
 
     st.success(f"Prediction: {predicted_class}")
-
     st.info(f"Confidence: {confidence:.2f}%")
