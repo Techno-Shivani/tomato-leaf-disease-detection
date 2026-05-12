04ae2a47329ee8e7550eb97961a65d89b6c3f42c
Shivani Chauhan <sc622732@gmail.com>
2026-05-12 09:28:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,24 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import gdown
+import os
+
+st.set_page_config(page_title="Tomato Leaf Disease Detection")
+
+# Google Drive Model Link
+MODEL_URL = "https://drive.google.com/uc?id=1pflxySlxBXUmLVOorwLU93MzJVHM16ub"
+
+MODEL_PATH = "best_model.keras"
+
+# Download model automatically
+if not os.path.exists(MODEL_PATH):
+    gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
 
 # Load model
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model("best_model.keras")
+    model = tf.keras.models.load_model(MODEL_PATH)
     return model
 
 model = load_model()
@@ -28,24 +41,13 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
 
-    image = Image.open(uploaded_file).convert("RGB")
+    image = Image.open(uploaded_file)
 
-    st.image(
-        image,
-        caption="Uploaded Image",
-        use_container_width=True
-    )
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    # Resize image
+    # Preprocess image
     img = image.resize((224, 224))
-
-    # Convert to array
-    img_array = np.array(img)
-
-    # Normalize
-    img_array = img_array / 255.0
-
-    # Expand dimensions
+    img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
     # Prediction
@@ -55,7 +57,6 @@ if uploaded_file is not None:
 
     confidence = np.max(prediction) * 100
 
-    # Result
     st.success(f"Prediction: {predicted_class}")
 
     st.info(f"Confidence: {confidence:.2f}%")
